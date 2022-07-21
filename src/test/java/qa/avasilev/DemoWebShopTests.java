43d03971bd4d2ec409b88d15c30c6c5706d55b89
Aleksei_Vasilev <aleksei.vasilev@iii.com>
2022-07-22 00:47:10 +0300

    Test harness created

--- src/test/java/qa/avasilev/DemoWebShopTests.java
+++ src/test/java/qa/avasilev/DemoWebShopTests.java
@@ -1,45 +1,31 @@
-import com.codeborne.selenide.Configuration;
+package qa.avasilev;
+
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
+import qa.avasilev.config.Project;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selectors.*;
-import static helpers.CustomApiListener.withCustomTemplates;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static qa.avasilev.helpers.AllureRestAssuredFilter.withCustomTemplates;
 
-public class DemoWebShopTests {
+public class DemoWebShopTests extends TestBase {
 
-    static String login = "qaguru@qa.guru",
-            password = "qaguru@qa.guru1",
+    static String login = Project.config.login(),
+            password = Project.config.password(),
             authCookieName = "NOPCOMMERCE.AUTH",
             authCookieValue,
             productId;
-
-    @BeforeAll
-    static void configure() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        Configuration.headless = true;
-        Configuration.baseUrl = "http://demowebshop.tricentis.com";
-        RestAssured.baseURI = "http://demowebshop.tricentis.com";
-    }
-
-    @AfterEach
-    void afterEach() {
-        closeWebDriver();
-    }
 
     @Test
     @DisplayName("Successful authorization to some demowebshop (UI)")
@@ -62,19 +48,19 @@ public class DemoWebShopTests {
     void loginWithApiTest() {
 
         step("Authorize via API and get the cookie", () -> {
-                    authCookieValue = given()
-                            .filter(withCustomTemplates())
-                            .contentType("application/x-www-form-urlencoded")
-                            .formParam("Email", login)
-                            .formParam("Password", password)
-                            .log().all()
-                            .when()
-                            .post("/login")
-                            .then()
-                            .log().all()
-                            .statusCode(302)
-                            .extract().cookie(authCookieName);
-                });
+            authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+        });
 
         step("Open minimal content, because cookie can be set when site is opened", () ->
                 open("/Themes/DefaultClean/Content/images/logo.png"));
@@ -126,15 +112,16 @@ public class DemoWebShopTests {
                     .body("success", is(true));
         });
 
-        step("Open minimal content, because cookie can be set when site is opened", () ->
-                open("/Themes/DefaultClean/Content/images/logo.png"));
-
-        step("Set cookie to to browser", () -> {
-            Cookie authCookie = new Cookie(authCookieName, authCookieValue);
-            WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
-        });
-
         step("Open cart and check, that the product is there, also get it's id", () -> {
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+
             open("/cart");
             assertNotEquals("(0)", $(".cart-qty").text());
             productId = productLink.parent().parent().$(".remove-from-cart").$("input").getValue();
@@ -142,31 +129,32 @@ public class DemoWebShopTests {
         });
 
         step("Remove added product via API", () -> {
-                    given()
-                            .filter(withCustomTemplates())
-                            .cookie(authCookieName, authCookieValue)
-                            .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)))
-                            .urlEncodingEnabled(true)
-                            .log().all()
-                            .formParam("removefromcart", productId)
-                            .formParam("itemquantity" + productId, 1)
-                            .formParam("updatecart", "Update shopping cart")
-                            .when()
-                            .post("/cart")
-                            .then()
-                            .log().all()
-                            .statusCode(200);
-                });
-
-        step("Open minimal content, because cookie can be set when site is opened", () ->
-                open("/Themes/DefaultClean/Content/images/logo.png"));
-
-        step("Set cookie to to browser", () -> {
-            Cookie authCookie = new Cookie(authCookieName, authCookieValue);
-            WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            given()
+                    .filter(withCustomTemplates())
+                    .cookie(authCookieName, authCookieValue)
+                    .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)))
+                    .urlEncodingEnabled(true)
+                    .log().all()
+                    .formParam("removefromcart", productId)
+                    .formParam("itemquantity" + productId, 1)
+                    .formParam("updatecart", "Update shopping cart")
+                    .when()
+                    .post("/cart")
+                    .then()
+                    .log().all()
+                    .statusCode(200);
         });
 
         step("Open browser again and check, that the product is deleted", () -> {
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+
             open("/cart");
             productLink.shouldNot(exist);
         });
---
+++ src/test/java/qa/avasilev/TestBase.java
@@ -0,0 +1,47 @@
+package qa.avasilev;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import qa.avasilev.config.Project;
+import qa.avasilev.helpers.AllureAttachments;
+import qa.avasilev.helpers.DriverSettings;
+import qa.avasilev.helpers.DriverUtils;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        DriverSettings.configure();
+        RestAssured.baseURI = Configuration.baseUrl;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
